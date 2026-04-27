f1c9aaaf8f021402be99d2cd653acab08c78d733
Mamed Guseinov <mameguseynov@mail.ru>
2026-04-27 15:39:03 +0300

    refactor: change events contracts source package

--- services/alert-service/internal/app/app.go
+++ services/alert-service/internal/app/app.go
@@ -7,13 +7,26 @@ import (
 	"syscall"
 
 	"github.com/childofemptiness/alert-service/internal/config"
+	inbox "github.com/childofemptiness/alert-service/internal/events"
 	"github.com/childofemptiness/alert-service/internal/storage/postgres"
+	"github.com/childofemptiness/monitoring-system/contracts/events"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"golang.org/x/sync/errgroup"
 )
 
+type Consumer interface {
+	Consume(ctx context.Context, handler func(ctx context.Context, event events.EventEnvelope) error) error
+}
+
+type InboxService interface {
+	HandleEvent(ctx context.Context, event events.EventEnvelope) error
+}
+
 type App struct {
-	cfg *config.Config
-	db  *pgxpool.Pool
+	cfg      *config.Config
+	db       *pgxpool.Pool
+	svc      InboxService
+	consumer Consumer
 }
 
 func New(
@@ -25,9 +38,13 @@ func New(
 		return nil, err
 	}
 
+	repo := postgres.NewInboxRepository(pgPool)
+	svc := inbox.NewInboxService(repo)
+
 	return &App{
 		cfg: cfg,
 		db:  pgPool,
+		svc: &svc,
 	}, nil
 }
 
@@ -35,11 +52,23 @@ func (a *App) Run() error {
 	rootCtx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
 
+	g, ctx := errgroup.WithContext(rootCtx)
+
 	log.Println("application started")
 
-	<-rootCtx.Done()
+	g.Go(func() error {
+		return a.consumer.Consume(ctx, a.svc.HandleEvent)
+	})
 
-	return nil
+	g.Go(func() error {
+		<-rootCtx.Done()
+
+		log.Println("application stopped")
+
+		return nil
+	})
+
+	return g.Wait()
 }
 
 func (a *App) Close() error {
